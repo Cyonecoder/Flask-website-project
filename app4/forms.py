f6cd3542dfc6a7c5aa7ac91135504813bc35533a
cyone <khassal.mohamed1@gmail.com>
2021-09-25 22:26:53 +0100

    add config models first structured files

--- app4/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-
-class NameForm(FlaskForm):
-    uname = StringField("What is your name?", validators=[Required()])
-    submit = SubmitField('Submit')
